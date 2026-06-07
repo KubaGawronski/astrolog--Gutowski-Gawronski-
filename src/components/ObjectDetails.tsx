2c683813c4a9dfbf8c37a14164643073aa34b90a
Kuba Gawronski <jakub.gawronski@cyfroweszkoly.pl>
2026-06-07 20:59:38 +0200

    Add object image, name, type and distance from earth

--- src/components/ObjectDetails.tsx
+++ src/components/ObjectDetails.tsx
@@ -9,7 +9,40 @@ function ObjectDetails({ selectedObject }: ObjectDetailsProps) {
         <div className="details-panel">
             <h2>PANEL OBSERWACYJNY</h2>
 
+            {selectedObject ? (
+                <>
+                    <div className="details-image-container">
+                        <img
+                            src={selectedObject.image}
+                            alt={selectedObject.name}
+                            className="details-image"
+                        />
+                    </div>
 
+                    <div className="details-table">
+                        <div className="details-row">
+                            <span>Nazwa:</span>
+                            <span>{selectedObject.name}</span>
+                        </div>
+
+                        <div className="details-row">
+                            <span>Typ obiektu:</span>
+                            <span>{selectedObject.type}</span>
+                        </div>
+
+                        <div className="details-row">
+                            <span>Odległość od Ziemi:</span>
+                            <span>{selectedObject.distance}</span>
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <div className="details-message">
+                    Wybierz pozycję z katalogu,
+                    <br />
+                    aby zobaczyć jej szczegóły.
+                </div>
+            )}
         </div>
     );
 }
