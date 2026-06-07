26e1f580d27e68157ea56697384c66c6ce0c56c3
Kuba Gawronski <jakub.gawronski@cyfroweszkoly.pl>
2026-06-07 20:58:25 +0200

    Add ObjectDetails function and div for ObjectDetails

--- src/components/ObjectDetails.tsx
+++ src/components/ObjectDetails.tsx
@@ -4,4 +4,14 @@ interface ObjectDetailsProps {
     selectedObject: AstroObject | null;
 }
 
+function ObjectDetails({ selectedObject }: ObjectDetailsProps) {
+    return (
+        <div className="details-panel">
+            <h2>PANEL OBSERWACYJNY</h2>
+
+
+        </div>
+    );
+}
+
 export default ObjectDetails;
