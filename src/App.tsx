2a85cd07516df45e2f33f9799065e5a8eb35d964
Gutek <jakub.gutowski@cyfroweszkoly.pl>
2026-06-08 14:39:03 +0200

    app.tsx structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,43 @@
-//import { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
+import ObjectList from "./components/ObjectList";
+import ObjectDetails from "./components/ObjectDetails";
+
+import { initialObjects } from "./data/initialObjects";
+import type { AstroObject } from "./types/AstroObject";
+
 function App() {
+    const [objects, setObjects] =
+        useState<AstroObject[]>(initialObjects);
 
+    const [selectedObject, setSelectedObject] =
+        useState<AstroObject | null>(null);
 
+    const handleSelectObject = (object: AstroObject) => {
+        setSelectedObject(object);
+    };
 
-  return (
-      <div className="app">
-        <header className="header">
-          <h1>AstroLog</h1>
-          <p>Katalog Ciał Niebieskich</p>
-        </header>
+    return (
+        <div className="app">
+            <header className="header">
+                <h1>AstroLog</h1>
+                <p>Katalog Ciał Niebieskich</p>
+            </header>
 
-      </div>
-  );
+            <main className="main-content">
+                <ObjectList
+                    objects={objects}
+                    onSelectObject={handleSelectObject}
+                />
+
+                <ObjectDetails
+                    selectedObject={selectedObject}
+                />
+
+            </main>
+        </div>
+    );
 }
 
 export default App;
